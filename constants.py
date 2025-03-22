c405f2b78790118cb2772efc9ad7ebdc453e8ede
jellybrigade <schachnermarcel@protonmail.com>
2025-03-22 15:54:49 +0100

    Removed debug/test files in other languages.

--- constants.py
+++
@@ -1,9 +0,0 @@
-# ...existing code...
-
-# Enemy Settings
-MAX_ENEMIES = 15  # Reduced from previous value
-ENEMY_SPAWN_INTERVAL = 180  # 3 seconds
-
-# UI Settings
-LEGEND_PANEL_WIDTH = 230
-LEGEND_PANEL_MARGIN = 10
